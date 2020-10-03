fd1fb5969d7cce0399be7bbcadb2945784b46a0c
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-10-03 12:05:08 +0530

    add 20_valid_parenthesis.py

--- 20_valid_parenthesis.py
+++ 20_valid_parenthesis.py
@@ -3,18 +3,20 @@
 class Solution:
 	def isValid(self, s:str) -> bool:
 		stack = []
-		open_list = ['(', '{', '[']
-		close_list = [')', '}', ']']
+		open = ['(', '{', '[']
+		close = [')', '}', ']']
 
 		for bracket in s:
-			if bracket in open_list:
+			if bracket in open:
 				stack.append(bracket)
-			elif bracket in close_list:
-				index = close_list.index(bracket)
-				if len(stack) > 0 and open_list[index] == stack[-1]:
+			elif bracket in close:
+				index = close.index(bracket)
+				
+				if len(stack) > 0 and open[index] == stack[-1]:
 					stack.pop()
 				else:
 					return False
-		if len(stack) == 0:
-			return True
-		return False
+
+			if len(stack) == 0:
+				return True
+			return False
